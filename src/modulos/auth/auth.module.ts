9a1b8b3da1eb0fce15ae5b9b15b804d7fdefae38
Francisco1asir <fparnav1908@g.educaand.es>
2024-01-23 13:28:35 +0100

    token2

--- src/modulos/auth/auth.module.ts
+++ src/modulos/auth/auth.module.ts
@@ -5,18 +5,33 @@ import { UserRepository } from '../user/entities/user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserRepository]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: 'claveSecreta123',
-      signOptions: {
-        expiresIn: '1h',
-        algorithm: 'HS256',
-      },
-    }),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '1h',
+    //     algorithm: 'HS256',
+    //   },
+    // }),
+    JwtModule.registerAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return{
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+          expiresIn: '1h',
+          algorithm: 'HS256', 
+          }
+        }
+      }
+
+    })
   ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
